3e6bd7314f33b391d747bce4834eaa93cbbb3f37
FurkannnAtes <101650106+FurkannnAtes@users.noreply.github.com>
2022-09-02 21:19:59 +0300

    logo fix

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -11,7 +11,7 @@ const Banner = () => {
         <section id="banner" className="banner-container  ">
 
             <div className="row h-100 w-100 ps-3 ps-lg-0">
-                <div className="banner-content col-md-6 text-white my-auto px-lg-5  ">
+                <div className="banner-content col-md-6 text-white my-auto px-lg-5  mb-5 mb-lg-0  ">
                     <div className='d-flex'>
                         <Slide left>  <div className="banner-title">01 </div></Slide>
                         <Roll left><div className='title-bar-black'></div></Roll>
@@ -25,7 +25,7 @@ const Banner = () => {
                     <button className="banner-content-button ">MINT TBA</button>
                 </div>
                 <div className="banner-logo col-md-6 my-auto text-center ">
-                    <img className='img-fluid h-100' src="/images/4.png" alt="" />
+                    <img className='img-fluid h-100' src="/images/3.jpg" alt="" />
                 </div>
             </div>
 
